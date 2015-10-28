59a86b0a1f263b551419e587bc7ac1d2e567e31c
Balsakup <balsa.ddress@gmail.com>
2015-10-28 17:07:16 +0100

    Ajout du fichier index.php
    Premier fichier du plugin. Dedans il y a toutes les informations sur le
    plugin ;)

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: ENSICAEN Digital Booklet
+Plugin URI: http://ensicaen-digital-booklet.github.io/Digital-Booklet-WP
+Description: Plugin Wordpress permettant d'ajouter le livret numérique de l'ENSICAEN à leur nouveau site sous Wordpress
+Version: 0.0.1-alpha
+Author: Quentin CATHERINE
+*/
