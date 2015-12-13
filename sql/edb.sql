9ca59926ee9243fe65e65e738179876d3ffc4e45
Balsakup <balsa.ddress@gmail.com>
2015-12-13 21:46:18 +0100

    L'affichage du début fait ;)
    Les spécialités, semestres, parcours et UEs sont affichés

--- sql/edb.sql
+++ sql/edb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 10 Décembre 2015 à 16:00
+-- Généré le :  Dim 13 Décembre 2015 à 21:44
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -27,17 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `wp_edb_langs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(2) NOT NULL,
-  PRIMARY KEY (`id`)
+  `lang_num` int(11) NOT NULL AUTO_INCREMENT,
+  `lang_name` varchar(50) NOT NULL,
+  `lang_code` varchar(2) NOT NULL,
+  PRIMARY KEY (`lang_num`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `wp_edb_langs`
 --
 
-INSERT INTO `wp_edb_langs` (`id`, `name`, `code`) VALUES
+INSERT INTO `wp_edb_langs` (`lang_num`, `lang_name`, `lang_code`) VALUES
 (1, 'Français', 'FR'),
 (2, 'Anglais', 'EN');
 
@@ -48,23 +48,45 @@ INSERT INTO `wp_edb_langs` (`id`, `name`, `code`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `wp_edb_schoolings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `major` tinyint(1) NOT NULL DEFAULT '0',
+  `schooling_type_num` int(11) NOT NULL,
   `speciality_num` int(11) NOT NULL,
   `semester_num` int(11) NOT NULL,
   `ue_num` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  PRIMARY KEY (`schooling_type_num`,`speciality_num`,`semester_num`,`ue_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `wp_edb_schoolings`
 --
 
-INSERT INTO `wp_edb_schoolings` (`id`, `major`, `speciality_num`, `semester_num`, `ue_num`) VALUES
-(1, 0, 1, 1, 1),
-(2, 0, 1, 1, 2),
-(3, 0, 2, 1, 3),
-(4, 0, 2, 1, 4);
+INSERT INTO `wp_edb_schoolings` (`schooling_type_num`, `speciality_num`, `semester_num`, `ue_num`) VALUES
+(1, 1, 1, 1),
+(1, 1, 1, 2),
+(1, 2, 1, 3),
+(1, 2, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `wp_edb_schooling_types`
+--
+
+CREATE TABLE IF NOT EXISTS `wp_edb_schooling_types` (
+  `schooling_type_num` int(11) NOT NULL,
+  `schooling_type_name` varchar(50) NOT NULL,
+  `lang_num` int(11) NOT NULL,
+  PRIMARY KEY (`lang_num`,`schooling_type_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `wp_edb_schooling_types`
+--
+
+INSERT INTO `wp_edb_schooling_types` (`schooling_type_num`, `schooling_type_name`, `lang_num`) VALUES
+(1, 'Tronc commun', 1),
+(2, 'Majeure', 1),
+(1, 'Common', 2),
+(2, 'Major', 2);
 
 -- --------------------------------------------------------
 
@@ -73,23 +95,22 @@ INSERT INTO `wp_edb_schoolings` (`id`, `major`, `speciality_num`, `semester_num`
 --
 
 CREATE TABLE IF NOT EXISTS `wp_edb_semesters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `semester_num` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `wording` varchar(100) NOT NULL,
-  `lang_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `semester_name` varchar(45) NOT NULL,
+  `semester_wording` varchar(100) NOT NULL,
+  `lang_num` int(11) NOT NULL,
+  PRIMARY KEY (`semester_num`,`lang_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `wp_edb_semesters`
 --
 
-INSERT INTO `wp_edb_semesters` (`id`, `semester_num`, `name`, `wording`, `lang_id`) VALUES
-(1, 1, 'Semestre 1', 'Semestre 1 à l''ENSICAEN', 1),
-(2, 2, 'Semestre 2', 'Semestre 2 à l''ENSICAEN', 1),
-(3, 1, 'Semester 1', 'Semester 1 at ENSICAEN', 2),
-(4, 2, 'Semester 2', 'Semester 2 at ENSICAEN', 2);
+INSERT INTO `wp_edb_semesters` (`semester_num`, `semester_name`, `semester_wording`, `lang_num`) VALUES
+(1, 'Semestre 1', 'Semestre 1 à l''ENSICAEN', 1),
+(1, 'Semester 1', 'Semester 1 at ENSICAEN', 2),
+(2, 'Semestre 2', 'Semestre 2 à l''ENSICAEN', 1),
+(2, 'Semester 2', 'Semester 2 at ENSICAEN', 2);
 
 -- --------------------------------------------------------
 
@@ -98,23 +119,23 @@ INSERT INTO `wp_edb_semesters` (`id`, `semester_num`, `name`, `wording`, `lang_i
 --
 
 CREATE TABLE IF NOT EXISTS `wp_edb_specialities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `speciality_num` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `wording` varchar(100) NOT NULL,
-  `lang_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `speciality_name` varchar(45) NOT NULL,
+  `speciality_wording` varchar(100) NOT NULL,
+  `speciality_order` int(11) DEFAULT NULL,
+  `lang_num` int(11) NOT NULL,
+  PRIMARY KEY (`speciality_num`,`lang_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `wp_edb_specialities`
 --
 
-INSERT INTO `wp_edb_specialities` (`id`, `speciality_num`, `name`, `wording`, `lang_id`) VALUES
-(1, 1, 'Informatique', 'Diplôme d''ingénieur Informatique', 1),
-(2, 2, 'Génie Industriel', 'Diplôme d''ingénieur Génie Industriel', 1),
-(3, 1, 'Computer science', 'Computer engineering degree', 2),
-(4, 2, 'Industrial Engineering', 'Engineering Degree Industrial Engineering', 2);
+INSERT INTO `wp_edb_specialities` (`speciality_num`, `speciality_name`, `speciality_wording`, `speciality_order`, `lang_num`) VALUES
+(1, 'Informatique', 'Diplôme d''ingénieur Informatique', -1, 1),
+(1, 'Computer science', 'Computer engineering degree', -1, 2),
+(2, 'Génie Industriel', 'Diplôme d''ingénieur Génie Industriel', -1, 1),
+(2, 'Industrial Engineering', 'Engineering Degree Industrial Engineering', -1, 2);
 
 -- --------------------------------------------------------
 
@@ -123,29 +144,29 @@ INSERT INTO `wp_edb_specialities` (`id`, `speciality_num`, `name`, `wording`, `l
 --
 
 CREATE TABLE IF NOT EXISTS `wp_edb_ues` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ue_num` int(11) NOT NULL,
-  `code` varchar(45) NOT NULL,
-  `wording` varchar(100) NOT NULL,
-  `average_min` float NOT NULL DEFAULT '10',
-  `ECTS` int(11) NOT NULL,
-  `lang_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+  `ue_code` varchar(45) NOT NULL,
+  `ue_wording` varchar(100) NOT NULL,
+  `ue_average_min` float NOT NULL DEFAULT '10',
+  `ue_ECTS` int(11) NOT NULL,
+  `ue_order` int(11) DEFAULT NULL,
+  `lang_num` int(11) NOT NULL,
+  PRIMARY KEY (`ue_num`,`lang_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `wp_edb_ues`
 --
 
-INSERT INTO `wp_edb_ues` (`id`, `ue_num`, `code`, `wording`, `average_min`, `ECTS`, `lang_id`) VALUES
-(1, 1, '1L1AA', 'Langues & Culture de l''Entreprise ', 10, 6, 1),
-(2, 2, '1I1AB', 'Mathematiques et signal', 10, 6, 1),
-(3, 3, 'ATM3', 'Initiation ', 10, 8, 1),
-(4, 4, 'GIA', 'Management et Communication', 10, 23, 1),
-(5, 1, '1L1AA', 'Languages & Culture of Enterprise', 10, 6, 2),
-(6, 2, '1L1AB', 'Mathematics and Signal', 10, 6, 2),
-(7, 3, 'ATM3', 'Initiation\r\n', 10, 8, 2),
-(8, 4, 'GIA', 'Management and Communication', 10, 23, 2);
+INSERT INTO `wp_edb_ues` (`ue_num`, `ue_code`, `ue_wording`, `ue_average_min`, `ue_ECTS`, `ue_order`, `lang_num`) VALUES
+(1, '1L1AA', 'Langues & Culture de l''Entreprise ', 10, 6, -1, 1),
+(1, '1L1AA', 'Languages & Culture of Enterprise', 10, 6, -6, 2),
+(2, '1I1AB', 'Mathematiques et signal', 10, 6, -1, 1),
+(2, '1L1AB', 'Mathematics and Signal', 10, 6, -1, 2),
+(3, 'ATM3', 'Initiation ', 10, 8, -1, 1),
+(3, 'ATM3', 'Initiation\r\n', 10, 8, -1, 2),
+(4, 'GIA', 'Management et Communication', 10, 23, -1, 1),
+(4, 'GIA', 'Management and Communication', 10, 23, -1, 2);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- src/template.php
+++ src/template.php
@@ -2,13 +2,78 @@
 
 global $wpdb;
 
-$edblang      = isset($_GET['edblang']) && !empty($_GET['edblang']) ? $_GET['edblang'] : 'FR';
-$lang_id      = $wpdb->get_row("SELECT id FROM {$wpdb->prefix}edb_langs WHERE code = '$edblang'")->id;
+$edblang         = isset($_GET['edblang']) && !empty($_GET['edblang']) ? $_GET['edblang'] : 'FR';
 
-$langs        = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_langs");
+global $lang_num;
+global $schooling_types;
 
-$specialities = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_specialities WHERE lang_id = $lang_id");
-$semesters    = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_semesters WHERE lang_id = $lang_id");
+$lang_num        = $wpdb->get_row("SELECT lang_num FROM {$wpdb->prefix}edb_langs        WHERE lang_code = '$edblang'")->lang_num;
+$langs           = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_langs");
+$specialities    = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_specialities    WHERE lang_num  = $lang_num");
+$semesters       = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_semesters       WHERE lang_num  = $lang_num");
+$schooling_types = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}edb_schooling_types WHERE lang_num  = $lang_num");
+
+function getSchooling($speciality_num, $semester)
+{
+	$schoolings      = array();
+	
+	global $lang_num;
+	global $schooling_types;
+	global $wpdb;
+	
+	foreach ($schooling_types as $schooling_type)
+	{
+		$schoolings[$schooling_type->schooling_type_name] = array( 
+			$wpdb->get_results("
+				SELECT * FROM wp_edb_schoolings
+				JOIN wp_edb_schooling_types USING(schooling_type_num)
+				JOIN wp_edb_specialities    USING(speciality_num, lang_num)
+				JOIN wp_edb_semesters       USING(semester_num, lang_num)
+				JOIN wp_edb_ues             USING(ue_num, lang_num)
+				WHERE
+				lang_num = (
+					SELECT lang_num FROM wp_edb_langs WHERE lang_num = $lang_num
+				)
+				AND
+				schooling_type_num = (
+					SELECT schooling_type_num FROM wp_edb_schooling_types WHERE schooling_type_name = '$schooling_type->schooling_type_name'
+				)
+				AND
+				speciality_num = $speciality_num
+				AND
+				semester_num = $semester
+			")
+		);
+	}
+
+
+	/*foreach ($schooling_types as $schooling_type)
+	{	
+		$schoolings[$schooling_type->schooling_type_name] = array( 
+			$wpdb->get_results("
+				SELECT * FROM wp_edb_schoolings
+				JOIN wp_edb_schooling_types USING(schooling_type_num)
+				JOIN wp_edb_specialities    USING(speciality_num, lang_num)
+				JOIN wp_edb_semesters       USING(semester_num, lang_num)
+				JOIN wp_edb_ues             USING(ue_num, lang_num)
+				WHERE
+				lang_num = (
+					SELECT lang_num FROM wp_edb_langs WHERE lang_num = $lang_num
+				)
+				AND
+				schooling_type_num = (
+					SELECT schooling_type_num FROM wp_edb_schooling_types WHERE schooling_type_name = '$schooling_type->schooling_type_name'
+				)
+				AND
+				speciality_num = $speciality_num
+				AND
+				semester_num = $semester
+			")
+		);
+	}*/
+	
+	return $schoolings;
+}
 
 echo '$_GET: <br>';
 echo '<pre>';
@@ -20,35 +85,69 @@ echo '</pre>';
 <nav class="edb_flags">
 	<?php foreach ($langs as $lang): ?>
 	
-	<a href="<?php echo esc_url(add_query_arg('edblang', $lang->code)); ?>"><img src="<?php echo plugins_url('assets/img/flags/' . $lang->code . '.png', __FILE__); ?>" alt="<?php echo $lang->name; ?>"></a>
+	<a href="<?php echo esc_url(add_query_arg('edblang', $lang->lang_code)); ?>"><img src="<?php echo plugins_url('assets/img/flags/' . $lang->lang_code . '.png', __FILE__); ?>" alt="<?php echo $lang->lang_name; ?>"></a>
 	
 	<?php endforeach; ?>
 </nav>
 
 <div id="edb" class="specialities">
-	<?php foreach ($specialities as $k => $speciality): ?>
+	<?php foreach ($specialities as $speciality): ?>
 	
 	<div class="item speciality">
-		<a href="#<?php echo sanitize_title($speciality->name) . '-' . $speciality->id; ?>" class="accordion_title"><?php echo $speciality->name; ?></a>
+		<a href="#<?php echo sanitize_title($speciality->speciality_name) . '-' . $speciality->speciality_num; ?>" class="accordion_title"><?php echo $speciality->speciality_name; ?></a>
 		
-		<div id="<?php echo sanitize_title($speciality->name) . '-' . $speciality->id; ?>" class="accordion_content">
+		<div id="<?php echo sanitize_title($speciality->speciality_name) . '-' . $speciality->speciality_num; ?>" class="accordion_content">
 			
 			<div class="semesters">
-				<?php foreach ($semesters as $k => $semester): ?>
+				<?php foreach ($semesters as $semester): ?>
 				
 				<div class="item semester">
-					<a href="#<?php echo sanitize_title($semester->name) . '-' . $semester->id; ?>" class="accordion_title"><?php echo $semester->name; ?></a>
+					<a href="#<?php echo sanitize_title($semester->semester_name) . '-' . $semester->semester_num; ?>" class="accordion_title"><?php echo $semester->semester_name; ?></a>
 					
-					<table>
+					<table class="ue_list">
 						<tbody>
+							<?php foreach (getSchooling($speciality->speciality_num, $semester->semester_num) as $schooling_name => $schoolings): ?>
+									
 							<tr>
-								<td>
-									<!-- Description UE -->
-								</td>
-								<td>
-									<!-- Modules de l'UE -->
+								<td colspan="2" class="eu_separator">
+									<span class="ue_type"><?php echo $schooling_name; ?></span>
 								</td>
 							</tr>
+							
+								<?php foreach (current($schoolings) as $schooling): print_r((array) $schooling); ?>
+								
+								<tr>
+									<td width="25%">
+										<span class="ue_code"><?php echo $schooling->ue_code; ?></span>
+										<span class="ue_name"><?php echo $schooling->ue_wording; ?> </span>
+										<span class="ue_average"><?php echo array_keys((array) $schooling, $schooling->ue_average_min) . ': ' . $schooling->ue_average_min; ?></span>
+										<span class="ue_ects"><?php echo array_keys((array) $schooling, $schooling->ue_ECTS) . ': ' . $schooling->ue_ECTS; ?></span>
+									</td>
+									<td width="75%">
+									</td>
+								</tr>
+								
+								<?php endforeach; ?>
+									
+							<?php endforeach; ?>
+							
+							<!--
+							<tr>
+								<td colspan="2" class="eu_separator">
+									<span class="ue_type">Tronc commun</span>
+								</td>
+							</tr>
+							<tr>
+								<td width="25%">
+									<span class="ue_code">1L1aA</span>
+									<span class="ue_name">Langues & Culture de l'Entreprise </span>
+									<span class="ue_average">10</span>
+									<span class="ue_ects">6</span>
+								</td>
+								<td width="75%">
+								</td>
+							</tr>
+							-->
 						</tbody>
 					</table>
 				</div>
